7e88048bcf424eea1e9b5ba3dd54d12052f84a12
twosky0202 <leehaneul0202@naver.com>
2023-08-30 15:19:58 +0900

    feat: 로그인한 사용자 별 일정, 일정 좋아요 조회 수정

--- src/main/java/com/hanium/diARy/plan/controller/UserPlanController.java
+++ src/main/java/com/hanium/diARy/plan/controller/UserPlanController.java
@@ -3,35 +3,44 @@ package com.hanium.diARy.plan.controller;
 import com.hanium.diARy.plan.dto.PlanResponseDto;
 import com.hanium.diARy.plan.service.PlanLikeService;
 import com.hanium.diARy.plan.service.PlanService;
+import com.hanium.diARy.user.entity.User;
+import com.hanium.diARy.user.repository.UserRepositoryInterface;
+import com.hanium.diARy.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/user/{userId}")
+@RequestMapping("/user")
 public class UserPlanController {
 
     private final PlanService planService;
     private final PlanLikeService planLikeService;
+    private final UserRepositoryInterface userRepositoryInterface;
 
-    public UserPlanController(PlanService planService, PlanLikeService planLikeService) {
+    public UserPlanController(PlanService planService, PlanLikeService planLikeService, UserRepositoryInterface userRepositoryInterface) {
         this.planService = planService;
         this.planLikeService = planLikeService;
+        this.userRepositoryInterface = userRepositoryInterface;
     }
 
     @GetMapping("/plan")
-    public ResponseEntity<List<PlanResponseDto>> getAllPlanByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<PlanResponseDto>> getAllPlanByUserId() {
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long userId = userRepositoryInterface.findByEmail(userEmail).getUserId();
         List<PlanResponseDto> planIdsByUser = planService.getAllPlanByUserId(userId);
         return new ResponseEntity<>(planIdsByUser, HttpStatus.OK);
     }
 
     @GetMapping("/plan-like")
-    public ResponseEntity<List<PlanResponseDto>> getAllPlanLikedByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<PlanResponseDto>> getAllPlanLikedByUserId() {
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long userId = userRepositoryInterface.findByEmail(userEmail).getUserId();
         List<PlanResponseDto> planIdsLikedByUser = planLikeService.getAllPlanLikedByUserId(userId);
         return new ResponseEntity<>(planIdsLikedByUser, HttpStatus.OK);
     }
