8e56c9510dd9f9510f7f46c546f7ae30af7fce2e
twosky0202 <leehaneul0202@naver.com>
2023-09-19 21:07:18 +0900

    feat: plan 삭제 시 take in 같이 삭제 되게 구현

--- src/main/java/com/hanium/diARy/plan/repository/PlanTakeInRepository.java
+++ src/main/java/com/hanium/diARy/plan/repository/PlanTakeInRepository.java
@@ -4,7 +4,11 @@ import com.hanium.diARy.plan.entity.PlanTakeIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlanTakeInRepository extends JpaRepository<PlanTakeIn, Long> {
     void deleteByPlan_PlanIdAndUser_UserId(Long planId, Long userId);
+
+    List<PlanTakeIn> findByPlan_PlanId(Long planId);
 }
