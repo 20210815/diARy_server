db0be299b3e4edc39093f00e795722ef9e47850c
이규민 <90364652+20210815@users.noreply.github.com>
2023-09-20 19:51:36 +0900

    핫토픽 수정

--- src/main/java/com/hanium/diARy/home/hottopic/service/TopicService.java
+++ src/main/java/com/hanium/diARy/home/hottopic/service/TopicService.java
@@ -33,7 +33,13 @@ public class TopicService {
 
     public List<HottopicDto> readBestDiaryTag() {
         //3순위까지만
-        List<DiaryTag> diaryTags = tagRepository.DescDiaryTag().subList(0, 3);
+        List<DiaryTag> diaryTags;
+        if (tagRepository.DescDiaryTag().size() <= 3) {
+            diaryTags = tagRepository.DescDiaryTag();
+        }
+        else {
+            diaryTags = tagRepository.DescDiaryTag().subList(0,3);
+        }
         List<HottopicDto> hottopicDtos = new ArrayList<>();
 
         for (DiaryTag diaryTag : diaryTags) {
